1bc23036be485d39ecf4f06e22f6543ca797b490
Jessica Stone <jstone4815@gmail.com>
2020-03-30 19:33:17 -0700

    create function with basic switch statement

--- calculator.js
+++ calculator.js
@@ -1,5 +1,27 @@
 const exampleAdditionInput = {
   num1: 3,
-  num2: 5,
-  operation: 'add',
+  num2: 0,
+  operation: '/',
 }
+
+const calculator = function(object) {
+  switch (object['operation']) {
+    case 'add':
+    case '+':
+      return object['num1'] + object['num2'];
+    case 'subtract':
+    case '-':
+      return object['num1'] - object['num2'];
+    case 'multiple':
+    case '*':
+      return object['num1'] * object['num2'];
+    case 'divide':
+    case '/':
+      return object['num1'] / object['num2'];
+    default:
+      return 'Invalid operator';
+  }
+
+}
+
+console.log(calculator(exampleAdditionInput));
